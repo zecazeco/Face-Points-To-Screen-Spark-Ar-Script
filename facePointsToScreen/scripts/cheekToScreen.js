6d436aab3766745eed228ca8c27c11a3a16fbe94
zecazeco <joseluishaas@gmail.com>
2021-09-28 16:57:43 -0300

    first commit

---
+++ facePointsToScreen/scripts/cheekToScreen.js
@@ -0,0 +1,42 @@
+
+const FaceTracking = require('FaceTracking');
+const Scene = require('Scene');
+const CameraInfo = require('CameraInfo');
+const Patches = require('Patches');
+const R = require('Reactive');
+const D = require('Diagnostics');
+
+
+// Enable async/await in JS [part 1]
+(async function() {
+
+	//const faceIndex = await Patches.outputs.getScalar('faceIndex');
+
+	const [face] = await Promise.all([
+		FaceTracking.face(0),
+	]);
+
+	const screenWidth = CameraInfo.previewSize.width;
+	const screenHeight = CameraInfo.previewSize.height;
+	const screenScale = CameraInfo.previewScreenScale;
+
+	const points = {
+		//leftCheek
+		leftCheekCenter: pointS(face.leftCheek.center),
+		leftCheekCheekbone: pointS(face.leftCheek.cheekbone),
+		//rightCheek
+		rightCheekCenter: pointS(face.rightCheek.center),
+		rightCheekCheekbone: pointS(face.rightCheek.cheekbone),	
+	};
+	
+	Object.keys(points).forEach((k) => {
+	    Patches.inputs.setPoint2D(k, R.div(R.point2d(points[k].x, R.sub(screenHeight, points[k].y)),screenScale))
+	    .catch(e => D.log('[EN] You should set '+k+' in From Script. Select the cheekToScreen.js file in the assets list, after that go to the right panel and click in + beside the From Script field, select Vector2 type and name it '+k+'\n[BR] Você deve definir um valor '+k+' em From Script. Selecione o arquivo cheekToScreen.js na lista de assets, após isto vá no painel a direita e clique em + ao lado do campo From Script, selecione o tipo Vector2 e chame de '+k));
+	});
+
+	function pointS(id) {
+		return Scene.projectToScreen(face.cameraTransform.applyToPoint(id));
+	}
+
+// Enable async/await in JS [part 2]
+})();
---
+++ facePointsToScreen/scripts/chinToScreen.js
@@ -0,0 +1,38 @@
+
+const FaceTracking = require('FaceTracking');
+const Scene = require('Scene');
+const CameraInfo = require('CameraInfo');
+const Patches = require('Patches');
+const R = require('Reactive');
+const D = require('Diagnostics');
+
+
+// Enable async/await in JS [part 1]
+(async function() {
+
+	//const faceIndex = await Patches.outputs.getScalar('faceIndex');
+
+	const [face] = await Promise.all([
+		FaceTracking.face(0),
+	]);
+
+	const screenWidth = CameraInfo.previewSize.width;
+	const screenHeight = CameraInfo.previewSize.height;
+	const screenScale = CameraInfo.previewScreenScale;
+
+	const points = {
+		chinTip: pointS(face.chin.tip),	
+	};
+	
+	Object.keys(points).forEach((k) => {
+	    Patches.inputs.setPoint2D(k, R.div(R.point2d(points[k].x, R.sub(screenHeight, points[k].y)),screenScale))
+	    .catch(e => D.log('[EN] You should set '+k+' in From Script. Select the chinToScreen.js file in the assets list, after that go to the right panel and click in + beside the From Script field, select Vector2 type and name it '+k+'\n[BR] Você deve definir um valor '+k+' em From Script. Selecione o arquivo chinToScreen.js na lista de assets, após isto vá no painel a direita e clique em + ao lado do campo From Script, selecione o tipo Vector2 e chame de '+k));
+	});
+
+	function pointS(id) {
+		return Scene.projectToScreen(face.cameraTransform.applyToPoint(id));
+	}
+
+// Enable async/await in JS [part 2]
+})();
+
